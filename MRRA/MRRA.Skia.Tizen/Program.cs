9c5866a762fbbe2567deece0d28e8b3976099db0
Stephen Odundo <steveremdi@gmail.com>
2021-04-04 16:28:30 +0300

    Add project files.

---
+++ MRRA/MRRA.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace MRRA.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new MRRA.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ MRRA/MRRA.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace MRRA.Skia.Gtk
+{
+}
